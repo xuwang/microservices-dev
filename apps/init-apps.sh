d7600f7172d1f196dbde648b7eb8598890f2bff1
Xu Wang <xuwang@gmail.com>
2015-04-13 00:41:16 -0700

    use https://www.testshib.org to test auth-mellon

--- apps/init-apps.sh
+++ apps/init-apps.sh
@@ -3,13 +3,19 @@
 # Start initial services at boot
 # NOTE: the start order is significant
 SERVICES="
-	registry
-	redis
-	mongo
+	#registry	
+	#redis
+	#mongo
+	#fleet-ui
+	#auth
 	"
 for svs in $SERVICES ; do
 	unit=/var/lib/apps/$svs/units/$svs.service
 	if [ -f $unit ]; then
+		if [ ! $(fleetctl list-units | grep $unit grep $unit > /dev/null 2>&1) ]; then
+			echo fleetctl destroy $unit
+			fleetctl destroy $unit > /dev/null 2>&1
+		fi
 		echo fleetctl start $unit
 		fleetctl start $unit
 	fi

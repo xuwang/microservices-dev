1126193fff39af2568bb9da2e2729f9804ab9617
Xu Wang <xuwang@gmail.com>
2015-08-14 20:06:51 -0700

    fix apps/grafana/data/import.py to prevent inserting existing datapoints again

--- apps/grafana/data/import.py
+++ apps/grafana/data/import.py
@@ -17,6 +17,7 @@ import datetime
 from docopt import docopt
 import requests
 from requests.auth import HTTPBasicAuth
+from contextlib import closing
 
 # Data source
 DATA_SOURCE='google'
@@ -28,50 +29,62 @@ PORT = '8086'
 DB = 'sp500'
 DB_USER = 'root'
 DB_PASS = 'root@work'
-ENDPOINT = 'write'
+DB_WRITE = 'write'
+DB_QUERY = 'query'
 PRECISION='s'
-DB_URL = "http://%s:%s/%s?db=%s&precision=%s" \
-    % (HOST, PORT, ENDPOINT, DB, PRECISION)
+DB_WRITE_URL = "http://%s:%s/%s?db=%s&precision=%s" % (HOST, PORT, DB_WRITE, DB, PRECISION)
+DB_QUERY_URL = "http://%s:%s/%s?db=%s&precision=%s" % (HOST, PORT, DB_QUERY, DB, PRECISION)
 
 
 def main(args):
     if args['<symbol>']:
         stock = args['<symbol>']
         try:
-            data = get_data(stock)
-            for line in data.splitlines():
-                if line[0].isdigit():
-                    add_datapoint(stock, line.rstrip())
+            with closing(requests.get(DATA_SOURCE_URL+stock, stream=True)) as r:
+                if r.status_code != 200:
+                    #print r.text
+                    raise ValueError("Error: Symbol Not Found")
+                else:
+                    for line in r.iter_lines():
+                        if line[0].isdigit():
+                            datapoint = marshall_datapoint(stock, line.rstrip())              
+                            add_datapoint(datapoint)
         except ValueError,e:
             print(e)
             print(__doc__)
             sys.exit(1)
 
-def get_data(symbol):
-    r = requests.get(DATA_SOURCE_URL+symbol)
+def get_datapoint(measurement, t):
+    query = "select * from %s where time=%ss" % (measurement, t)
+    params = {"q":query}
+    r = requests.get(DB_QUERY_URL, auth=(DB_USER, DB_PASS), params=params)
     if r.status_code != 200:
-        #print r.text
-        raise ValueError("Error: Symbol Not Found")
+        raise ValueError("Error: %s %s" % r.status_code, r.text)
     else:
         return r.text
                     
-def add_datapoint(measurement, data):
-    post_data = marshall_datapoint(measurement, data)
-    #print post_data
-    r = requests.post(DB_URL, auth=(DB_USER, DB_PASS), data=post_data)
-    if r.status_code != 204:
-        print r.status_code, r.text
+def add_datapoint(data):
+    dp = get_datapoint(data["measurement"], data["time"])
+    # insert the datapoint iff it's not in the db
+    if "time" in dp:
+        print "Datapoint exist: ", dp
     else:
-        print "Datapoint added: ", post_data
+        post_data ="%s,%s %s %s" \
+            % (data["measurement"], data["tags"], data["fields"], data["time"])
+        #print post_data
+        r = requests.post(DB_WRITE_URL, auth=(DB_USER, DB_PASS), data=post_data)
+        if r.status_code != 204:
+            print r.status_code, r.text
+        else:
+            print "Datapoint added: ", post_data
 
 def marshall_datapoint(measurement, data):
     v_date, v_open, v_high, v_low, v_close, v_volume = data.split(',')
-    v_time = time.mktime(datetime.datetime.strptime(v_date, "%d-%b-%y").timetuple())
+    v_time = time.mktime(datetime.datetime.strptime(v_date + " 17:00:00", "%d-%b-%y %H:%M:%S").timetuple())
     v_time = int(v_time)
     tags = "source=%s" % DATA_SOURCE
-    fields = "open=%s,high=%s,low=%s,close=%s,volume=%s" \
-        % (v_open, v_high, v_low, v_close, v_volume)
-    return "%s,%s %s %s" % (measurement, tags, fields, v_time)
+    fields = "open=%s,high=%s,low=%s,close=%s,volume=%s" % (v_open, v_high, v_low, v_close, v_volume)
+    return {"measurement":measurement, "tags":tags, "fields":fields, "time":v_time}
 
 if __name__ == '__main__':
     args = docopt(__doc__)
